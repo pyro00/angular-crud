63e59b64775e9171ad3e76fc65d512c744861d2e
Asım Emre Kaya <ekayadesign16@gmail.com>
2019-07-22 18:06:47 +0300

    Angular Crud
    Create Read Update Delete
    Local Strorage

---
+++ src/app/navbar/navbar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.css']
+})
+export class NavbarComponent implements OnInit {
+  // tslint:disable-next-line: ban-types
+  isCollapsed: Boolean = true;
+  constructor() { }
+  ngOnInit() {
+  }
+
+}
